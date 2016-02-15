e4988402085c60f2b8c5ee964378157bf6969429
Riadi <asyaririadi@gmail.com>
2016-02-15 14:53:28 +0700

    modified commit

---
+++ app/src/main/java/com/example/riadi/navigationview/Address.java
@@ -0,0 +1,14 @@
+package com.example.riadi.navigationview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by riadi on 2/15/2016.
+ */
+public class Address extends Activity{
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_3);
+    }
+}
